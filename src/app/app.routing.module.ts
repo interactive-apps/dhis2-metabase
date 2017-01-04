307da8f548983c4794ecaa1bdc17b1fbdb05a659
Rajabu Mkomwa <rajeyw@gmail.com>
2017-01-04 16:46:27 +0300

    removed unwanted modules

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule }     from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadChildren: 'app/home/home.module#HomeModule'},
-  { path: 'feature', loadChildren: 'app/feature-module/feature-module.module#FeatureModule'}
+  { path: '', loadChildren: 'app/home/home.module#HomeModule'}
+
 ];
 
 @NgModule({
--- src/app/feature-module/feature-module.routing.module.ts
+++
@@ -1,7 +0,0 @@
-import { ModuleWithProviders }  from '@angular/core';
-import { RouterModule } from '@angular/router';
-import {FeatureModuleComponent} from "./feature-module.component";
-
-export const FeatureRoutingModule: ModuleWithProviders = RouterModule.forChild([
-  {path: '', component: FeatureModuleComponent}
-]);
